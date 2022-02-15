3795a10db525e064a86baac4d8c496d4942290c0
Flying Santas <88877972+Reindears@users.noreply.github.com>
2022-02-15 17:43:17 +0530

    Update rename.py

--- bot/plugins/rename.py
+++ bot/plugins/rename.py
@@ -31,7 +31,7 @@ from bot.core.handlers.big_rename import handle_big_rename
 async def renamestart(c: Client, m: Message): 
     replied_m = m
     file_name = get_media_file_name(replied_m)
-    text = f"**File Name:** `{_file_name}`\n\n" \
+    text = f"**File Name:** `{file_name}`\n\n" \
                f"**File Extension:** `{_file_name.rsplit('.', 1)[-1].upper()}`\n\n" \
                f"**File Type:** `{get_file_type(replied_m).upper()}`\n\n" \
                f"**File Size:** `{humanbytes(get_media_file_size(replied_m))}`\n\n" \
