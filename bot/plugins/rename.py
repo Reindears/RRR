540b9b906485bcd0923c3437b1c5fc826e9fd392
Flying Santas <88877972+Reindears@users.noreply.github.com>
2022-02-15 18:08:03 +0530

    Update rename.py

--- bot/plugins/rename.py
+++ bot/plugins/rename.py
@@ -18,7 +18,6 @@ from bot.core.file_info import (
     get_media_file_size,
     get_media_file_name,
     get_file_type,
-    get_media_caption,
     get_file_attr
 )
 from configs import Config
@@ -35,7 +34,7 @@ async def renamestart(c: Client, m: Message):
     _file_name = get_media_file_name(replied_m)
     text = f"**File Name:** `{_file_name}`\n\n" \
                f"**File Extension:** `{_file_name.rsplit('.', 1)[-1].upper()}`\n\n" \
-               f"**File Type:** `{get_media_caption(replied_m).upper()}`\n\n" \
+               f"**File Type:** `{get_file_type(replied_m).upper()}`\n\n" \
                f"**File Size:** `{humanbytes(get_media_file_size(replied_m))}`\n\n" \
                f"**File MimeType:** `{get_file_attr(replied_m).mime_type}`"
 
@@ -49,7 +48,6 @@ async def renamestart(c: Client, m: Message):
         disable_web_page_preview=True,
         reply_to_message_id=m.message_id
     )
-
 
 
 
