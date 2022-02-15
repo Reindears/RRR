447a932efc83cfd3381fe2c8a9e4797d852042ac
Flying Santas <88877972+Reindears@users.noreply.github.com>
2022-02-15 21:18:13 +0530

    Update callbacks.py

--- bot/plugins/callbacks.py
+++ bot/plugins/callbacks.py
@@ -146,7 +146,7 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
         await cb.message.delete(True)
         
     elif cb.data == "rename":
-        editable = await c.send_message(cb.message.chat.id, text="Now send me new file name", reply_markup = ForceReply())
+        editable = await c.send_message(cb.message.chat.id, text="Now send me new file name", reply_to_message_id=message.reply_to_message.message_id, reply_markup = ForceReply())
         user_input_msg: Message = await c.listen(cb.message.chat.id)
         if user_input_msg.text is None:
           await editable.edit("Process Cancelled!")
