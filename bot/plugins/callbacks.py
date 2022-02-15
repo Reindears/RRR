1c42fda8373ee27d2528180d2a56db748ddcd257
Flying Santas <88877972+Reindears@users.noreply.github.com>
2022-02-15 12:25:50 +0530

    Update callbacks.py

--- bot/plugins/callbacks.py
+++ bot/plugins/callbacks.py
@@ -131,20 +131,20 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
     if user_input_msg.text and user_input_msg.text.startswith("/"):
         await editable.edit("Process Cancelled!")
         return await user_input_msg.continue_propagation()
-    _raw_file_name = get_media_file_name(m.reply_to_message)
+    _raw_file_name = get_media_file_name(cb.reply_to_message)
     if not _raw_file_name:
-        _file_ext = mimetypes.guess_extension(get_file_attr(m.reply_to_message).mime_type)
+        _file_ext = mimetypes.guess_extension(get_file_attr(cb.reply_to_message).mime_type)
         _raw_file_name = "UnknownFileName" + _file_ext
     if user_input_msg.text.rsplit(".", 1)[-1].lower() != _raw_file_name.rsplit(".", 1)[-1].lower():
         file_name = user_input_msg.text.rsplit(".", 1)[0][:255] + "." + _raw_file_name.rsplit(".", 1)[-1].lower()
     else:
         file_name = user_input_msg.text[:255]
     await editable.edit("Please Wait ...")
-    is_big = get_media_file_size(m.reply_to_message) > (10 * 1024 * 1024)
+    is_big = get_media_file_size(cb.reply_to_message) > (10 * 1024 * 1024)
     if not is_big:
         _default_thumb_ = await db.get_thumbnail(m.from_user.id)
         if not _default_thumb_:
-            _m_attr = get_file_attr(m.reply_to_message)
+            _m_attr = get_file_attr(cb.reply_to_message)
             _default_thumb_ = _m_attr.thumbs[0].file_id \
                 if (_m_attr and _m_attr.thumbs) \
                 else None
