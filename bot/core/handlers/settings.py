8b132a8bde91fdf0ceb03fd005867d4ccb098527
Flying Santas <88877972+Reindears@users.noreply.github.com>
2022-02-15 21:02:19 +0530

    Update settings.py

--- bot/core/handlers/settings.py
+++ bot/core/handlers/settings.py
@@ -17,18 +17,18 @@ async def show_settings(m: "types.Message"):
     apply_caption = user_data.get("apply_caption", True)
     thumbnail = user_data.get("thumbnail", None)
     buttons_markup = [
-        [types.InlineKeyboardButton(f"🗂 Upload - {'Document' if upload_as_doc else 'Video'}",
+        [types.InlineKeyboardButton(f"Upload - {'Document' if upload_as_doc else 'Video'}",
                                     callback_data="triggerUploadMode")],
-        [types.InlineKeyboardButton("🖼 Set Thumbnail",
+        [types.InlineKeyboardButton("Set Thumbnail",
                                     callback_data="setThumbnail")],
-        [types.InlineKeyboardButton("➕ Add  Caption",
+        [types.InlineKeyboardButton("Add  Caption",
                                     callback_data="setCustomCaption")]
     ]
     if thumbnail:
-        buttons_markup.append([types.InlineKeyboardButton("🏞 Show Thumbnail",
+        buttons_markup.append([types.InlineKeyboardButton("Show Thumbnail",
                                                           callback_data="showThumbnail")])
     if caption:
-        buttons_markup.append([types.InlineKeyboardButton("📮 Show Caption",
+        buttons_markup.append([types.InlineKeyboardButton("Show Caption",
                                                           callback_data="showCaption")])
     buttons_markup.append([types.InlineKeyboardButton("❌ Close",
                                                       callback_data="closeMessage")])
