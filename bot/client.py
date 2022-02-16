3edc1b6bba52b7c05e3d6430da28c0e6f4736504
Flying Santas <88877972+Reindears@users.noreply.github.com>
2022-02-16 22:34:38 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -20,6 +20,7 @@ class Client(RawClient, New):
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
+            workers=200,
             plugins=dict(
                 root="bot/plugins"
             )
